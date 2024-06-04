708fa388fd6b1568f1131b49fa788939b8fd0b7d
lucic15 <lucicluka31@gmail.com>
2024-06-05 01:43:55 +0200

    Embeds and more commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const {
   joinVoiceChannel,
   createAudioPlayer,
@@ -20,15 +20,23 @@ const commands = [
   },
   {
     name: "!play",
-    description: "Plays given playlist!",
+    description: "Plays the given playlist!",
   },
   {
     name: "!p",
-    description: "Temp!",
+    description: "Plays a predefined playlist!",
+  },
+  {
+    name: "!pause",
+    description: "Pauses the music for a specified time!",
+  },
+  {
+    name: "!unpause",
+    description: "Resumes the paused music!",
   },
   {
     name: "!next",
-    description: "Skips current song!",
+    description: "Skips the current song!",
   },
   {
     name: "!stop",
@@ -57,7 +65,6 @@ async function playPlaylist(playlistUrl, message) {
   try {
     let playlist = await play.playlist_info(playlistUrl, { incomplete: true });
     audioPlayer = createAudioPlayer();
-
     videos = playlist.videos;
     if (shuffleMode) {
       videos = shuffleArray(videos);
@@ -81,20 +88,35 @@ async function playPlaylist(playlistUrl, message) {
 
       audioPlayer.play(resource);
 
-      const nowPlayingMessage = await message.channel.send(
-        `Now Playing: **${video.title}** | 00:00 / ${video.durationRaw}`
-      );
+      const musicEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle(`Now Playing: **${video.title}**`)
+        .setAuthor({
+          name: "Music Bot",
+          // Temp icon
+          iconURL:
+            "https://raw.githubusercontent.com/codetheweb/aoede/main/.github/logo.png",
+        })
+        .setDescription(`00:00 / ${video.durationRaw}`)
+        .setImage(`${video.thumbnails[3].url}`)
+        .setTimestamp()
+        .setFooter({
+          text: "Some footer text here",
+        });
 
-      console.log(video);
+      const nowPlayingMessage = await message.channel.send({
+        embeds: [musicEmbed],
+      });
 
       connection.subscribe(audioPlayer);
 
       const interval = setInterval(() => {
-        nowPlayingMessage.edit(
-          `Now Playing: **${video.title}** | ${formatTime(
-            audioPlayer.state.playbackDuration
-          )} / ${video.durationRaw}`
+        const updatedEmbed = EmbedBuilder.from(musicEmbed).setDescription(
+          `${formatTime(audioPlayer.state.playbackDuration)} / ${
+            video.durationRaw
+          }`
         );
+        nowPlayingMessage.edit({ embeds: [updatedEmbed] });
       }, 1000);
 
       audioPlayer.once("idle", () => {
@@ -106,7 +128,7 @@ async function playPlaylist(playlistUrl, message) {
     playNextVideo();
   } catch (error) {
     console.error(error);
-    message.channel.reply("There was an error trying to play the playlist!");
+    message.reply("There was an error trying to play the playlist!");
   }
 }
 
@@ -125,6 +147,21 @@ async function stopPlaying() {
   }
 }
 
+async function pause(timeout) {
+  if (audioPlayer) {
+    audioPlayer.pause();
+    if (timeout) {
+      setTimeout(() => unpause(), timeout * 1000);
+    }
+  }
+}
+
+async function unpause() {
+  if (audioPlayer) {
+    audioPlayer.unpause();
+  }
+}
+
 async function next() {
   if (connection && audioPlayer) {
     if (shuffleMode) {
@@ -140,10 +177,27 @@ function toggleShuffle(message) {
 }
 
 function help(message) {
-  const helpMessage = commands
-    .map((cmd) => `\`${cmd.name}\`: ${cmd.description}`)
-    .join("\n");
-  message.reply(helpMessage);
+  const helpEmbed = new EmbedBuilder()
+    .setColor(0x0099ff)
+    .setTitle("Help Menu")
+    .setAuthor({
+      name: "Music Bot",
+      iconURL:
+        "https://raw.githubusercontent.com/codetheweb/aoede/main/.github/logo.png",
+    })
+    .addFields(
+      commands.map((cmd) => ({
+        name: cmd.name,
+        value: cmd.description,
+        inline: false,
+      }))
+    )
+    .setTimestamp()
+    .setFooter({
+      text: "Some footer text here",
+    });
+
+  message.channel.send({ embeds: [helpEmbed] });
 }
 
 function formatTime(milliseconds) {
@@ -158,36 +212,31 @@ async function processCommand(command, args, message) {
     case "!help":
       help(message);
       break;
-
     case "!play":
       if (args.length < 2) {
         return message.reply("You need to provide a YouTube playlist URL!");
       }
-
       const playlistUrl = args[1];
       await playPlaylist(playlistUrl, message);
       break;
-
     case "!p":
-      await playPlaylist(
-        //.env variable lol
-        process.env.defined_url,
-        message
-      );
+      await playPlaylist(process.env.defined_url, message);
       break;
-
+    case "!pause":
+      pause(args[1]);
+      break;
+    case "!unpause":
+      unpause();
+      break;
     case "!stop":
       stopPlaying();
       break;
-
     case "!next":
       next();
       break;
-
     case "!shuffle":
       toggleShuffle(message);
       break;
-
     default:
       break;
   }
